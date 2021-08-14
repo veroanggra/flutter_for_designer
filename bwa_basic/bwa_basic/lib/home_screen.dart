589a3c0ee963402cba9bc8f1288c1323afbd9bff
veroanggra <veronicaputri497@gmail.com>
2021-08-14 12:10:30 +0700

    add scroll

--- bwa_basic/bwa_basic/lib/home_screen.dart
+++ bwa_basic/bwa_basic/lib/home_screen.dart
@@ -7,79 +7,81 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('assets/beach.jpeg'),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Pantai Teluk Penyu',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Cilacap, Jawa Tengah',
-                        style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.w300),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.favorite, color: Colors.yellow),
-                      Text('4.2')
-                    ],
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('assets/beach.jpeg'),
+              SizedBox(
+                height: 30,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.call, color: Colors.lightBlue),
-                      SizedBox(height: 8.0),
-                      Text('CALL')
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.send, color: Colors.lightBlue),
-                      SizedBox(height: 8.0),
-                      Text('ROUTE')
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.share, color: Colors.lightBlue),
-                      SizedBox(height: 8.0),
-                      Text('SHARE')
-                    ],
-                  )
-                ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Pantai Teluk Penyu',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Cilacap, Jawa Tengah',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.favorite, color: Colors.yellow),
+                        Text('4.2')
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                'Teluk Penyu merupakan kawasan pantai di selatan Kabupaten Cilacap, utamanya sepanjang pesisir dari Kecamatan Cilacap Selatan yang lokasinya tidak langsung berhubungan dengan Samudera India atau Indonesia karena dikelilingi oleh Pulau Nusakambangan. Pantai Teluk Penyu berjarak 2 Km ke arah timur dari Pusat Pemerintahan Kabupaten Cilacap dan dapat dijangkau dengan kendaraan umum dan pribadi. Teluk ini cukup memiliki pemandangan yang indah dengan luas kira-kira 14 ha.\nArea Teluk Penyu yang biasa dikunjungi oleh para pengunjung (utamanya penduduk dan wisatawan lokal) biasanya mulai dari pelabuhan perikanan Samudera dari hingga bibir pantai yang biasa disebut Areal 70 (merujuk kepada sebutan masyarakat sekitar terhadap kawasan tangki-tangki penimbunan bahan bakar dari PT Pertamina UP IV) dimana para wisatawan atau pengunjung bisa melihat langsung Pulau Nusakambangan dari bibir pantai.',
-                textAlign: TextAlign.justify,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.call, color: Colors.lightBlue),
+                        SizedBox(height: 8.0),
+                        Text('CALL')
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.send, color: Colors.lightBlue),
+                        SizedBox(height: 8.0),
+                        Text('ROUTE')
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.share, color: Colors.lightBlue),
+                        SizedBox(height: 8.0),
+                        Text('SHARE')
+                      ],
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Teluk Penyu merupakan kawasan pantai di selatan Kabupaten Cilacap, utamanya sepanjang pesisir dari Kecamatan Cilacap Selatan yang lokasinya tidak langsung berhubungan dengan Samudera India atau Indonesia karena dikelilingi oleh Pulau Nusakambangan. Pantai Teluk Penyu berjarak 2 Km ke arah timur dari Pusat Pemerintahan Kabupaten Cilacap dan dapat dijangkau dengan kendaraan umum dan pribadi. Teluk ini cukup memiliki pemandangan yang indah dengan luas kira-kira 14 ha.\nArea Teluk Penyu yang biasa dikunjungi oleh para pengunjung (utamanya penduduk dan wisatawan lokal) biasanya mulai dari pelabuhan perikanan Samudera dari hingga bibir pantai yang biasa disebut Areal 70 (merujuk kepada sebutan masyarakat sekitar terhadap kawasan tangki-tangki penimbunan bahan bakar dari PT Pertamina UP IV) dimana para wisatawan atau pengunjung bisa melihat langsung Pulau Nusakambangan dari bibir pantai.',
+                  textAlign: TextAlign.justify,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
